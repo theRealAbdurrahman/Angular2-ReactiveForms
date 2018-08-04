fe6d25620f668499b077a56af107a0c1aab3ef51
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-08-04 11:32:26 +0200

    * updated app.module to use the reactive forms module. * created a reactive form group and form controls * updated the template

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {  FormGroup, FormControl } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -7,11 +7,34 @@ import { Customer } from './customer';
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent  implements OnInit{
+    customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    ngOnInit() {
+        this.customerForm = new FormGroup({
+            firstName : new FormControl(),
+            lastName : new FormControl(),
+            email : new FormControl(),
+            sendCatalog : new FormControl(true),
+        })
     }
- }
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+    setDefaultData(){
+        this.customerForm.setValue({
+            firstName:"bedo",
+            lastName:"maged",
+            email:"bedo@maged.com",
+            sendCatalog: true,
+        })
+    }
+    setSomeData(){
+        this.customerForm.patchValue({
+            firstName:"bedo",
+            lastName:"maged",
+        })
+    }
+ } 
